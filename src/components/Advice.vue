88753b9bb8281708dcca5c7cf36526e5189bb35d
Rafał Zapotoczny <zapotoczny169@wp.pl>
2022-03-13 18:18:55 +0100

    add other filse

---
+++ src/components/Advice.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="advice">
+    <p>"{{ advice }}"</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['advice']
+}
+</script>
+
+<style>
+.advice {
+  padding: 0 22px 25px 22px;
+}
+
+p {
+  color: hsl(193, 38%, 86%);
+  letter-spacing: -0.5px;
+  font-size: 1.75rem;
+}
+</style>
